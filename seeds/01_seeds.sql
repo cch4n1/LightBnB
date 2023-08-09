dd3ec33926f64368d79697a1d57a355303b7bcae
Charlie C <zbyabqrc@pm.me>
2023-08-09 00:04:48 +0000

    updated data values

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,14 +1,17 @@
 INSERT INTO users (name, email, password)
-VALUES ('Michael Jackson', 'billiejean@jackson5.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'), 
-('Frodo Baggins', 'frodo@thehobbit.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'), 
-('Donald Trump', 'thedonald@trump.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+VALUES ('Darth Vader', 'vader@thegalacticempire.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), 
+('Luke Skywalker', 'luke@rebelalliance.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), 
+('Lando Calrissian', 'lando@cloudcity.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Jabba the Hutt', 'jabba@jabbaspalace.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');;
 
 INSERT INTO properties (owner_id , title , description , thumbnail_photo_url , cover_photo_url , cost_per_night , parking_spaces , number_of_bathrooms , number_of_bedrooms , country , street , city , province , post_code , active)
-  VALUES (1, 'Neverland Ranch', 'description' , 'https://thumbnail_photo_url' , 'https://cover_photo_url' , 50000 , 500 , 30 , 33 , 'United States' , '5225 Figueroa Mountain Rd' , 'Santa Ynez' , 'California' , '93460' , true), 
-  (2, 'Bag End', 'description' , 'https://thumbnail_photo_url' , 'https://cover_photo_url' , 350 , 2 , 1 , 3 , 'Middle Earth' , '1 Bag End' , 'The Shire' , 'Eriador' , '1111' , true);
+  VALUES (1, 'The Death Star', 'description' , 'https://thumbnail_url' , 'https://cover_url' , 500 , 1 , 1 , 2 , 'Galactic Empire', 'Galactic Orbit', 'Death Star', 'Despayre' , '99999' , true), 
+  (2, 'Skywalker Ranch', 'description' , 'https://thumbnail_url' , 'https://cover_url' , 350 , 2 , 1 , 3 , 'Tattoine' , '5858 Lucas Valley Rd' , 'Mos Eisley', 'Dune Sea', '12345', true),
+  (3, 'Cloud City', 'description', 'https://thumbnail_url', 'https://cover_url', 200, 1, 1, 1, 'Bespin', '123 Cloud Ave', 'Cloud City', 'Cloud Province', '56789', true),
+  (4, 'Jabbas Palace', 'description', , 'https://thumbnail_url', 'https://cover_url', 100, 0, 0, 0, 'Tatooine', '456 Dune St', 'Hutt City', 'Dune Sea', '88888', true);
 
 INSERT INTO reservations (start_date ,  end_date  , property_id , guest_id)
   VALUES ('2018-09-11', '2018-09-26', 1, 3);
 
 INSERT INTO property_reviews (guest_id , property_id , reservation_id , rating , message)
-  VALUES (3, 1, 1, 3, 'it was tremendous')
+  VALUES (3, 1, 1, 3, 'fantastic')
